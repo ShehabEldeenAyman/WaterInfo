6f0c126f2036183acd8c5cd713635e6de45643b5
ShehabEldeenAyman <shehab.ayman@vito.be>
2025-08-18 10:04:06 +0200

    Update RDF2TSS.py

--- TimeSeriesSnippets/RDF2TSS.py
+++ TimeSeriesSnippets/RDF2TSS.py
@@ -15,7 +15,6 @@ def LoadGraph(directory):
 
 def CreateSensorSet(graph):
     sensor_set = set()
-    sensor_set.clear()
     get_sensor_query = ''' 
 PREFIX sosa: <http://www.w3.org/ns/sosa/>
 
@@ -24,97 +23,101 @@ WHERE {
   ?s sosa:madeBySensor ?sensor .
 }
 '''
-    print('Started identifying unique senors')
+    print('Started identifying unique sensors')
+    # store actual RDF terms (URIRef or Literal), not stringified values
     for sensor in graph.query(get_sensor_query):
-        #print(sensor.sensor)
-        sensor_set.add(str(sensor[0]))
-    
+        sensor_term = sensor[0]   # this is an rdflib term (URIRef or Literal)
+        sensor_set.add(sensor_term)
+
     print('Sensors identified successfully')
     return sensor_set
 
-def CreateTSS(sensor_set,graph):
+
+
+def CreateTSS(sensor_set, graph):
     prefix_tss = Namespace('https://w3id.org/tss#')
     prefix_ex  = Namespace('http://example.org/')
     prefix_sosa  = Namespace('http://www.w3.org/ns/sosa/')
     final_graph = Graph()
-    final_graph.bind('tss',prefix_tss)
-    final_graph.bind('ex',prefix_ex)
-    final_graph.bind('sosa',prefix_sosa)
+    final_graph.bind('tss', prefix_tss)
+    final_graph.bind('ex', prefix_ex)
+    final_graph.bind('sosa', prefix_sosa)
     print('Started creating final graph')
+
     for sensor in sensor_set:
-            tss_points = []
-            tss_query = f'''
-            PREFIX sosa: <http://www.w3.org/ns/sosa/>
+        # use sensor.n3() so SPARQL receives a proper token: <uri> or "literal"^^xsd:...
+        sensor_token = sensor.n3() if hasattr(sensor, 'n3') else f"<{str(sensor)}>"
 
-            SELECT ?READING ?TIME ?OBSERVATION ?observedProperty
-            WHERE {{
-                ?OBSERVATION a sosa:Observation ;
-                sosa:resultTime ?TIME;
-                sosa:hasSimpleResult ?READING;
-                sosa:observedProperty ?observedProperty;
-                sosa:madeBySensor <{sensor}>.
+        tss_points = []
+        tss_query = f'''
+        PREFIX sosa: <http://www.w3.org/ns/sosa/>
 
-            }}
-
-            ORDER BY ?TIME
-            '''
-            results = graph.query(tss_query)
-            for row in results:
-                #print(sensor, ' value:' ,row.READING, ' time:', row.TIME, ' ID:' ,row.OBSERVATION, ' Observed property: ',row.observedProperty)
-
-                
-                data = {
-                    'time': row.TIME,
-                    'value': row.READING,
-                    'id': row.OBSERVATION,
-                    #'observedProperty' :  row.observedProperty
-                }
-                
-                tss_points.append(data)
-            json_object = json.dumps(tss_points) #serialize json object to a string
-            
-            #Create new graph 
-            subject  = prefix_ex[f"snippet/{str(tss_points[0]['time'])[:9]}"] #this is the proper subject and should replace "URIRef(sensor)"
-            #point_template = BNode(f'${URIRef(sensor)}') #blank node for the PointTemplate. the sensor uri reference is added here to make sure that temporary node has unique id
-            #print(point_template) #for testing 
-            temporary_node = BNode() #temporary node is created for each sensor at a time to ensure its uniqueness.
-
-            final_graph.add((URIRef(sensor),RDF.type,prefix_tss.Snippet)) #temp
-            final_graph.add((URIRef(sensor),prefix_tss.points,Literal(json_object, datatype=RDF.JSON))) #the json array with time, value, id.
-            final_graph.add((URIRef(sensor),prefix_tss["from"],tss_points[0]['time'])) #from is a reserved word, hence worked around it this way
-            final_graph.add((URIRef(sensor),prefix_tss.to,tss_points[-1]['time'])) #from is a reserved word, hence worked around it this way
-            final_graph.add((URIRef(sensor),prefix_tss.pointType,prefix_sosa.Observation)) 
-
-            #tss context json object
-            context_obj = {
-            "@context": {
-                "id": "@id",
-                "time": {
-                    "@id": "http://www.w3.org/ns/sosa/resultTime",
-                    "@type": "http://www.w3.org/2001/XMLSchema#dateTime"
-                },
-                "value": {
-                    "@id": "http://www.w3.org/ns/sosa/resultTime",
-                    "@type": "http://www.w3.org/2001/XMLSchema#integer"
-                }
+        SELECT ?READING ?TIME ?OBSERVATION ?observedProperty
+        WHERE {{
+            ?OBSERVATION a sosa:Observation ;
+                         sosa:resultTime ?TIME ;
+                         sosa:hasSimpleResult ?READING ;
+                         sosa:observedProperty ?observedProperty ;
+                         sosa:madeBySensor {sensor_token} .
+        }}
+        ORDER BY ?TIME
+        '''
+        results = graph.query(tss_query)
+        for row in results:
+            # each row values are rdflib terms; keep them
+            data = {
+                'time': row.TIME,
+                'value': row.READING,
+                'id': row.OBSERVATION,
+                'observedProperty': row.observedProperty
             }
-        }
-            context_obj = json.dumps(context_obj) #serialize json object to a string
-            #final_graph.add((URIRef(sensor),prefix_tss.context,Literal(context_obj, datatype=RDF.JSON))) #this part is static. It is decided to remove it from each instance. It will be added later on a higher level.
-            
-            #temporary node part
-            final_graph.add((URIRef(sensor),prefix_tss.about,temporary_node))
-            final_graph.add((temporary_node, RDF.type, prefix_tss.PointTemplate))
-            final_graph.add((temporary_node,prefix_sosa.madeBySensor,URIRef(sensor)))
-            final_graph.add((temporary_node,prefix_sosa.observedProperty,row.observedProperty))
+            tss_points.append(data)
 
+        if not tss_points:
+            print(f'Warning: no observations found for sensor {sensor_token}; skipping.')
+            continue  # avoid indexing into empty list
 
-            '''
-            print(json_object)
-            print('start time: ', tss_points[0]['time']) #since readings are already sorted, first object holds start time
-            print('end time: ', tss_points[-1]['time']) #while last object always holds last time
-            print('-----------------------------------------------------------------------------------------------------------------------')
-            '''
+        json_object = json.dumps([
+            {
+                'time': str(p['time']),    # convert to string for JSON
+                'value': str(p['value']),
+                'id': str(p['id'])
+            }
+            for p in tss_points
+        ])
+
+        # choose an output subject: use the real URI if sensor is URIRef,
+        # otherwise mint an example URI for that sensor value
+        if isinstance(sensor, URIRef):
+            subject = sensor
+        else:
+            # for a Literal sensor (e.g. "24002042"), create a stable URI in ex: namespace
+            safe_id = str(sensor).replace(' ', '_')
+            subject = prefix_ex[f"sensor/{safe_id}"]
+
+        temporary_node = BNode()
+
+        final_graph.add((subject, RDF.type, prefix_tss.Snippet))
+        # store the JSON timeseries as a literal (you had RDF.JSON before; keep as plain literal or use a string)
+        final_graph.add((subject, prefix_tss.points, Literal(json_object)))
+        # Add from / to times using the original RDF time Literals
+        final_graph.add((subject, prefix_tss["from"], tss_points[0]['time']))
+        final_graph.add((subject, prefix_tss.to, tss_points[-1]['time']))
+        final_graph.add((subject, prefix_tss.pointType, prefix_sosa.Observation))
+
+        # tss context omitted as in your original comment
+
+        final_graph.add((subject, prefix_tss.about, temporary_node))
+        final_graph.add((temporary_node, RDF.type, prefix_tss.PointTemplate))
+        # madeBySensor should point to original sensor term if it was a URI, otherwise to an identifier URI
+        if isinstance(sensor, URIRef):
+            final_graph.add((temporary_node, prefix_sosa.madeBySensor, sensor))
+        else:
+            # point to an identifier URI we just created
+            final_graph.add((temporary_node, prefix_sosa.madeBySensor, subject))
+        # use observedProperty from the first point (assumes same observedProperty for sensor)
+        final_graph.add((temporary_node, prefix_sosa.observedProperty, tss_points[0]['observedProperty']))
+
     print('Graph created successfully')
     return final_graph
 
